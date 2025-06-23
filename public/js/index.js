765d101c0847076fc51892a34340469268a0fb85
Lindsey <lindseyfuller@outlook.com>
2025-06-23 13:51:54 -0400

    added code in index.js

---
+++ public/js/index.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    },
+    
+        {
+        quote: "I think of art as the highest level of creativity.",
+        author: "David Rockefeller",
+    },
+
+        {
+        quote: "The worst enemy to creativity is self-doubt.",
+        author: "Sylvia Plath",
+    },
+
+        {
+        quote: "A true artist is not one who is inspired, but one who inspires others.",
+        author: "Salvador Dali",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
