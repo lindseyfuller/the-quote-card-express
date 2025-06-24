1f80fd74d9433e9d61fff086df68204d100c09e6
Lindsey <lindseyfuller@outlook.com>
2025-06-24 15:45:30 -0400

    installed dotenv and added code to server.js

--- server.js
+++ server.js
@@ -3,9 +3,19 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use("/api/v1/getRandomImage", (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: process.env.CLIENT_ID
+    });
+});
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
